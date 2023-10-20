f89e4ee4fb308723731a8c98918c86ac67e219f9
Vini <vini@gmail.com>
2023-10-20 13:07:53 -0300

    Insert

--- scr/mod_clientes/cliente.py
+++ scr/mod_clientes/cliente.py
@@ -1,7 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
+from settings import HEADERS_API, ENDPOINT_CLIENTE
+from funcoes import Funcoes
+
 bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 ''' rotas dos formulários '''
-@bp_cliente.route('/cliente/')
+# @bp_cliente.route('/cliente/')
+# def formListaCliente():
+#     return render_template('formListaCliente.html'), 200
+
+@bp_cliente.route('/', methods=['GET', 'POST'])
 def formListaCliente():
-    return render_template('formListaCliente.html'), 200
+    try:
+        response = requests.get(ENDPOINT_CLIENTE, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        return render_template('formListaCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+@bp_cliente.route('/form-cliente/', methods=['POST'])
+def formCliente():
+    return render_template('formCliente.html')
+
+@bp_cliente.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+        
+
+        # monta o JSON para envio a API
+        payload = {'id_CLIENTE': id_cliente, 'nome': nome, 'cpf': cpf, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        print(result) # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code) # 200
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        
+        return render_template('cliente.formListaCliente.html', msg=result[0])
+
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
--- scr/mod_funcionario/funcionario.py
+++ scr/mod_funcionario/funcionario.py
@@ -1,11 +1,61 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
+from settings import HEADERS_API, ENDPOINT_FUNCIONARIO
+from funcoes import Funcoes
+
+
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
 ''' rotas dos formulários '''
-@bp_funcionario.route('/funcionario/')
+@bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
-    return render_template('formListaFuncionarios.html'), 200
+    try:
+        response = requests.get(ENDPOINT_FUNCIONARIO, headers=HEADERS_API)
+        result = response.json()  
 
-@bp_funcionario.route('/form-funcionario/', methods=['GET'])
+        print (ENDPOINT_FUNCIONARIO)
+
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        return render_template('formListaFuncionarios.html', result=result[0])   
+    
+    except Exception as e:
+        return render_template('formListaFuncionarios.html', msgErro=e.args[0])
+        
+    
+
+@bp_funcionario.route('/form-funcionario/', methods=['POST'])
 def formFuncionario():
     return render_template('formFuncionario.html')
+    
+
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        # monta o JSON para envio a API
+        payload = {'id_FUNCIONARIO': id_funcionario, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        
+        response = requests.post(ENDPOINT_FUNCIONARIO, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        print(result) # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code) # 200
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        
+        return render_template('funcionario.formListaFuncionarios.html', msg=result[0])
+    
+    except Exception as e:
+        return render_template('formListaFuncionarios.html', msgErro=e.args[0])
